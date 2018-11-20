eed0ed055f597839a6bc4862145b3a647342374b
Mark Dalrymple <markd@bignerdranch.com>
2018-11-19 21:54:31 -0500

    Print an announcement in didFinishLaunching

--- Pascal/AppDelegate.swift
+++ Pascal/AppDelegate.swift
@@ -13,15 +13,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        print("OOPACK")
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
 
