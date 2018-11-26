8ce966dbafe16952952da1176b7622894ad24a17
Mark Dalrymple <markd@bignerdranch.com>
2018-11-25 21:09:25 -0500

    Adds -2 case (first time) and base case (char at index)

--- Pascal/Source.swift
+++ Pascal/Source.swift
@@ -10,13 +10,13 @@ class Source {
     static let EOF: Char = "\0"
 
     // private BufferedReader reader;   // reader for the source program
-    private var line = ""		// source line
-    private var lineNum = 0		// current source line number
-    private var currentPosition = 0	// current source line position
+    private var line: String?           // source line
+    private var lineNum = 0             // current source line number
+    private var currentPosition = 0     // current source line position
     private let reader: BufferedReader
 
     init(reader: BufferedReader) {
-        line = ""
+        line = nil
         lineNum = 0
         currentPosition = -2 // set to -2 to read the first source line. (?)
         self.reader = reader
@@ -27,8 +27,17 @@ class Source {
     // it's their BufferedReader that's doing that. So we're not throwing _(yet)_
     // Returns EOL/EOF at end of line/file
     func currentChar() -> Char {
-        // TODO - more
-        return "x"
+        if currentPosition == -2 {
+            readLine()
+            let next = nextChar()
+            return next
+        }
+
+        guard let line = line else {
+            preconditionFailure("unexpected nil line when expecting text")
+        }
+        let index = line.index(line.startIndex, offsetBy: currentPosition)
+        return String(line[index])
     }
 
     /// Consume the current source character and return the next one.
@@ -45,7 +54,11 @@ class Source {
 
     /// Reads the next source line
     private func readLine() {
-        // TODO - more
+        line = try! reader.readLine()
+        currentPosition = -1
+        if line != nil {
+            lineNum += 1
+        }
     }
 
     /// Closes the source
