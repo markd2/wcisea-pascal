430845c30dcaf2dd3dfd983afa557eae1f0781bb
Mark Dalrymple <markd@bignerdranch.com>
2018-11-25 21:12:32 -0500

    Refactors creation of the systemUnderTest.

--- PascalTests/SourceTests.swift
+++ PascalTests/SourceTests.swift
@@ -2,9 +2,14 @@ import XCTest
 @testable import Pascal
 
 class SourceTests: XCTestCase {
-    func testReadFirstChar() {
-        let reader = StringReader("bork")
+    private func sourceWith(string: String) -> Source {
+        let reader = StringReader(string)
         let source = Source(reader: reader)
+        return source
+    }
+
+    func testReadFirstChar() {
+        let source = sourceWith(string: "bork")
         let firstPost = source.currentChar()
         XCTAssertEqual(firstPost, "b")
     }
