e1baaf39abe2bfb48ae72a9f8063a9b2a992b5a9
Mark Dalrymple <markd@bignerdranch.com>
2018-11-25 20:35:53 -0500

    Adds failing test for currentChar

--- PascalTests/SourceTests.swift
+++ PascalTests/SourceTests.swift
@@ -2,4 +2,10 @@ import XCTest
 @testable import Pascal
 
 class SourceTests: XCTestCase {
+    func testReadFirstChar() {
+        let reader = StringReader("bork")
+        let source = Source(reader: reader)
+        let firstPost = source.currentChar()
+        XCTAssertEqual(firstPost, "b")
+    }
 }
