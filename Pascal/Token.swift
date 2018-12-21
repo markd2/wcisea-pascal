1a06f64d8178f0ff672d2cde0a773554c8917e79
Mark Dalrymple <markd@bignerdranch.com>
2018-12-20 22:30:10 -0500

    Adds some comments

--- Pascal/Token.swift
+++ Pascal/Token.swift
@@ -5,9 +5,12 @@ protocol TokenType { }
 /// The framework class that represents a token returned by the scanner.
 class Token {
     // java version are `protected`
-    var type: TokenType? // language-sepcific toke type
     var text: String?    // token text
+
+    // ??? Could this be generic-driven rather than type+anyobject?
+    var type: TokenType? // language-sepcific token type
     var value: Any?      // token value
+
     var lineNumber: Int
     var position: Int
     private var source: Source
@@ -20,11 +23,15 @@ class Token {
         extract()
     }
 
+    /// `extract` does the actual work of constructing the token,
     /// Default method to exract only one-character tokens from the source.
     /// Subclasses can override this to construct language-specific  tokens.
     /// After extracting the token, the current source line position will
     /// be one beyond the last token character.
     /// (java version threw if an error occurred.)
+    ///
+    /// subclasses are expected to leave the current source line position 
+    /// _one beyond_ the las token character (c.f. page 25)
     func extract() {
         text = String(currentChar())
         value = nil
